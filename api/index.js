087e82a0d8b6664ff6a8f14444c0c56c2944b416
Magaerv <marielaramirezvalle@gmail.com>
2024-05-10 14:38:16 -0300

    user and auth routes

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import mongoose from 'mongoose'
+import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 dotenv.config()
-
-const app = express()
 
 mongoose
   .connect(process.env.MONGO)
@@ -15,9 +15,15 @@ mongoose
     console.log(err)
   })
 
+
+const app = express()
+
+app.use(express.json())
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
 })
 
 
-
+app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
