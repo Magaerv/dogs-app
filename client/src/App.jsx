0b75388e998a0468b8159b4d55fbedb419c1f870
Magaerv <marielaramirezvalle@gmail.com>
2024-05-13 12:02:47 -0300

    private route and profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import { PrivateRoutes } from './components/PrivateRoutes'
+import About from './pages/About'
 import Home from './pages/Home'
+import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
-import About from './pages/About'
-import Profile from './pages/Profile'
-import Header from './components/Header'
 
 export default function App() {
   return <BrowserRouter>
@@ -14,7 +15,9 @@ export default function App() {
       <Route path='/sign-in' element={<SignIn />} />
       <Route path='/sign-up' element={<SignUp />} />
       <Route path='/about' element={<About />} />
-      <Route path='/profile' element={<Profile />} />
+      <Route element={PrivateRoutes}>
+        <Route path='/profile' element={<Profile />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 }
